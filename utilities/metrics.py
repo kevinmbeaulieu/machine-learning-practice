Fix pearson correlation computation

utilities/metrics.py
@@ -142,8 +142,15 @@ def _compute_r2(actual: np.ndarray, predicted: np.ndarray) -> float:
     return 1 - (ss_residual / ss_total)
 
 def _compute_pearson(actual: np.ndarray, predicted: np.ndarray) -> float:
-    return np.corrcoef(actual, predicted)
+    # Formula source: https://en.wikipedia.org/wiki/Pearson_correlation_coefficient#For_a_sample
+    mean_actual = actual.mean()
+    mean_predicted = predicted.mean()
+    numerator = ((actual - mean_actual) * (predicted - mean_predicted)).sum()
+    denominator = np.sqrt(((actual - mean_actual) ** 2).sum() * ((predicted - mean_predicted) ** 2).sum())
 
+    if denominator == 0:
+        return math.nan
+    return numerator / denominator
 
 def _sklearn_compute_metric(actual, predicted, metric: str, positive_class: str=None) -> float:
     if metric == 'acc':
@@ -187,4 +194,4 @@ def _sklearn_compute_r2(actual: np.ndarray, predicted: np.ndarray) -> float:
     return sklearn_metrics.r2_score(actual, predicted)
 
 def _sklearn_compute_pearson(actual: np.ndarray, predicted: np.ndarray) -> float:
-    return sklearn_feature_selection.r_regression(actual, predicted)
+    return sklearn_feature_selection.r_regression(actual, predicted.ravel())

utilities/tests/test_metrics.py
@@ -32,12 +32,11 @@ class TestRegressionMetrics(TestMetrics):
 
         self.verify_metric(actual_prices, predicted_prices, 'r2')
 
-    # TODO: Fix computation of pearson coefficient
-    # def test_compute_pearson(self):
-    #     actual_prices = self.df[['SalePrice']].to_numpy()
-    #     predicted_prices = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).reshape(10,1)
+    def test_compute_pearson(self):
+        actual_prices = self.df[['SalePrice']].to_numpy()
+        predicted_prices = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).reshape(10,1)
 
-    #     self.verify_metric(actual_prices, predicted_prices, 'pearson')
+        self.verify_metric(actual_prices, predicted_prices, 'pearson')
 
 class TestClassificationMetrics(TestMetrics):
     def setUp(self):