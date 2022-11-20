Continue trying to fix backprop

utilities/models/nn.py
@@ -9,11 +9,18 @@ class NeuralNetworkModel(Model):
     Neural Network Model.
     """
 
-    def __init__(self, batch_size: int = 32, num_epochs: int = 100, learning_rate: float = 0.01):
+    def __init__(
+        self,
+        batch_size: int = 32,
+        num_epochs: int = 100,
+        learning_rate: float = 0.01,
+        verbose: bool = False
+    ):
         self.batch_size = batch_size
         self.num_epochs = num_epochs
         self.learning_rate = learning_rate
         self.layers = []
+        self.verbose = verbose
 
     def train(self, df: pd.DataFrame, dataset: Dataset):
         if not isinstance(self.layers[0], InputLayer):
@@ -22,104 +29,127 @@ class NeuralNetworkModel(Model):
         self.dataset = dataset
         self.classes = df['class'].unique()
 
-        print("Training for {} epochs".format(self.num_epochs))
+        if self.verbose:
+            print("Training for {} epochs".format(self.num_epochs))
         for epoch in range(self.num_epochs):
-            print("Starting epoch {}...".format(epoch))
+            if self.verbose:
+                print("Starting epoch {}...".format(epoch))
             batches = self._get_batches(df)
-            print("  Got {} batches".format(len(batches)))
+            if self.verbose:
+                print("  Got {} batches".format(len(batches)))
             for batch_index, batch in enumerate(self._get_batches(df)):
-                print("  Training batch {}".format(batch_index))
+                if self.verbose:
+                    print("  Training batch {}".format(batch_index))
                 self._train_batch(batch)
 
     def _get_batches(self, df: pd.DataFrame) -> list[pd.DataFrame]:
         num_batches = np.ceil(df.shape[0] / self.batch_size).astype(int)
-        print("Creating {} batches".format(num_batches))
+        if self.verbose:
+            print("Creating {} batches".format(num_batches))
         return [df.sample(self.batch_size) for _ in range(num_batches)]
 
     def _train_batch(self, batch: pd.DataFrame):
-        Δw = np.zeros((len(self.layers,)))
-        Δb = np.zeros((len(self.layers,)))
-        for _, row in batch.iterrows():
-            Δw_t, Δb_t = self._train_row(row)
-            Δw += Δw_t
-            Δb += Δb_t
+        dW = []
+        for t, row in batch.iterrows():
+            dW_t = self._backpropagate(row, t)
+            if dW:
+                dW = [dw + self.learning_rate * dw_t for dw, dw_t in zip(dW, dW_t)]
+            else:
+                dW = [self.learning_rate * dw_t for dw_t in dW_t]
 
-        print("Δw:", Δw)
-        print("Δb:", Δb)
+        for i, layer in enumerate(self.layers[1:]):
+            layer.update_weights(dW[i])
+        print()
 
-    def _train_row(self, row: pd.Series) -> tuple[np.ndarray, np.ndarray]:
+    def _backpropagate(self, row: pd.Series, foo: int) -> list[np.ndarray]:
         """
         Train row via forward-propagation followed by back-propagation.
 
         :param row: Training example
-        :return: Tuple of changes to weights (first element) and biases for each layer (second element)
+        :return: Change in weights
         """
 
         # Notation:
-        #   row = input row t (= x^t = z_0^t)
+        #   row = input row t (= x^t = s_0^t = a_0^t)
         #
-        #   f_i(x) = activation function of layer i
-        #   f_i'(x) = derivative of activation function of layer i
+        #   L = number of layers
+        #   N_i = number of neurons in layer i
         #
-        #   z_i^t = output of layer i for x^t
-        #         = f_i(w_i^t • z_{i-1}^t + b_i^t)
-        #   y^t = output of last layer for x^t
-        #       = z_{L-1}^t
+        #   w_i^t = weights of layer i for x^t ((ℝ^N_i, ℝ^N_{i-1}))
+        #   b_i^t = bias of layer i for x^t (ℝ)
         #
-        #   e^t = error of last layer for x^t
-        #       = y\hat^t - y^t
-        #   e_i^t = error of layer i for x^t for i < L-1
+        #   f_i(x) = activation function of layer i (ℝ -> ℝ)
+        #   f_i'(x) = derivative of activation function of layer i (ℝ -> ℝ)
+        #
+        #   s_i^t = w_i^t • a_{i-1}^t + b_i^t (ℝ^N_i)
+        #   a_i^t = output of layer i for x^t (ℝ^N_i)
+        #         = f_i(s_i^t)
+        #   y^t = correct output of last layer for x^t (ℝ^N_{L-1})
+        #
+        #   e^t = error of last layer for x^t (ℝ^N_{L-1})
+        #       = a_i^t - y^t
+        #   e_i^t = error of layer i for x^t (ℝ^N_{i+1})
         #         = e_{i+1}^t • w_{i+1}^t
         #
-        #   η = learning rate
-        #   Δw_i^t = change in weights of layer i for x^t
-        #          = η • e_i^t • f_i'(z_i^t) • z_{i-1}^t
-        #   Δw_i = change in weights of layer i for this batch
-        #        = sum(Δw_i^t)
-        #   Δb_i^t = change in biases of layer i for x^t
-        #          = η • e_i^t • f_i'(z_i^t)
-        #   Δb_i = change in biases of layer i for this batch
-        #        = sum(Δb_i^t)
+        #   η = learning rate (ℝ)
+        #   Δw_i^t = change in weights of layer i for x^t (ℝ^N_i, ℝ^N_{i-1})
+        #          = η * a_{i-1}^t ⨂ e_i^t
+        #   Δw_i = change in weights of layer i for this batch (ℝ^N_i, ℝ^N_{i-1})
+        #        = Σ_t(Δw_i^t)
+        #   Δb_i^t = change in biases of layer i for x^t (ℝ^N_i)
+        #          = η * e_i^t
+        #   Δb_i = change in biases of layer i for this batch (ℝ^N_i)
+        #        = Σ_t(Δb_i^t)
 
         output_col = 'class' if self.dataset.task == 'classification' else 'output'
-        z = []
+        x_t = row.drop(output_col).values
+        a_t = []
         for layer in self.layers:
-            layer_inputs = (z[-1] if z else row.drop(output_col).values).astype('float')
-            z.append(layer.forward(layer_inputs))
+            layer_inputs = (a_t[-1] if a_t else x_t).astype('float')
+            a_t.append(layer.forward(layer_inputs))
 
-        # TODO: Encode output column for use with softmax before running through NN model.
-        y = row[output_col]
-        e = [z[-1] - y]
-        for layer_index in range(len(self.layers) - 1, 0, -1):
-            layer = self.layers[layer_index]
-            next_layer = self.layers[layer_index + 1]
-            next_layer_error = e[0]
-            e.insert(0, np.dot(next_layer_error, next_layer.weights.T))
+        correct_class = row[output_col]
+        y_t = pd.Series([1 if c == correct_class else 0 for c in self.classes], index=self.classes)
+        e_t = [a_t[-1] - y_t]
+        print(f"e_{foo}: {(e_t[0] ** 2).sum()}")
+        for k in range(len(self.layers) - 2, -1, -1):
+            layer_k = self.layers[k]
+            layer_kplus1 = self.layers[k + 1]
+            e_kplus1_t = e_t[0]
+            w_kplus1_t = layer_kplus1.weights
+            e_k_t = np.dot(e_kplus1_t, w_kplus1_t)
+            e_t.insert(0, e_k_t)
 
-        Δw = []
-        for i in range(1, len(self.layers)):
-            layer = self.layers[i]
-            Δw.append(η * e[i] * layer._activate_derivative(z[i]) * z[i - 1])
+        Δw_t = []
+        for k in range(1, len(self.layers) - 1):
+            layer = self.layers[k]
+            s_kmin1_t = layer._activation_fn_derivative(a_t[k - 1])
+            Δw_k_t = np.outer(s_kmin1_t, e_t[k]).T
+            Δw_t.append(Δw_k_t)
+        Δw_Lmin1_t = np.outer(a_t[-2], e_t[-1]).T
+        Δw_t.append(Δw_Lmin1_t)
 
-        Δb = []
-        for i in range(1, len(self.layers)):
-            layer = self.layers[i]
-            Δb.append(η * e[i] + layer._activate_derivative(z[i]))
-
-        return Δw, Δb
+        return Δw_t
 
     def predict(self, df: pd.DataFrame) -> pd.Series:
-        result = pd.Series(index=df.index)
+        result_dtype = 'str' if self.dataset.task == 'classification' else 'float'
+        result = pd.Series(index=df.index, dtype=result_dtype)
         for _, row in df.iterrows():
             result[row.name] = self._predict_row_class(row)
         return result
 
-    def _predict_row_class(self, row: pd.Series) -> str:
+    def _predict_row_class(self, row: pd.Series) -> any:
         output_col = 'class' if self.dataset.task == 'classification' else 'output'
-        values = row.drop(output_col).values
+
+        values = row.values
         for i, layer in enumerate(self.layers):
             values = layer.forward(values)
-        return self.classes[values.argmax()]
+
+        if self.dataset.task == 'classification':
+            print(values)
+            return self.classes[values.argmax()]
+        else:
+            return values
 
 class Layer:
     """
@@ -137,6 +167,14 @@ class Layer:
         :return: Outputs of the layer.
         """
         raise Exception("Must be implemented by subclass")
+
+    def update_weights(self, Δw: np.ndarray):
+        """
+        Update the weights of the layer.
+
+        :param Δw: Change in weights.
+        """
+        pass
 
 class InputLayer(Layer):
     """
@@ -172,7 +210,7 @@ class DenseLayer(Layer):
         self.activation = activation
         self.input_size = None
         self.weights = None
-        self.bias = None
+#         self.bias = None
 
     def forward(self, inputs: np.ndarray) -> np.ndarray:
         """
@@ -188,7 +226,8 @@ class DenseLayer(Layer):
             # Lazily construct weights to avoid having to specify input size on initialization.
             self._initialize_weights_and_bias()
 
-        return self._activate(np.dot(self.weights, inputs) + self.bias)
+#         return self._activate(np.dot(self.weights, inputs) + self.bias)
+        return self._activate(np.dot(self.weights, inputs))
 
     def _initialize_weights_and_bias(self):
         if self.activation == 'relu':
@@ -206,7 +245,7 @@ class DenseLayer(Layer):
         else:
             self.weights = np.random.randn(self.units, self.input_size)
 
-        self.bias = np.zeros((self.units,))
+#         self.bias = np.zeros((self.units,))
 
     def _activate(self, z: np.ndarray) -> np.ndarray:
         if self.activation == 'sigmoid':
@@ -216,11 +255,12 @@ class DenseLayer(Layer):
         elif self.activation == 'tanh':
             return np.tanh(z)
         elif self.activation == 'softmax':
+            z -= np.max(z) # Avoid overflow.
             return np.exp(z) / np.sum(np.exp(z))
         elif self.activation == 'linear':
             return z
 
-    def _activate_derivative(self, z: np.ndarray) -> np.ndarray:
+    def _activation_fn_derivative(self, z: np.ndarray) -> np.ndarray:
         if self.activation == 'sigmoid':
             return np.exp(z) / (1 + np.exp(z)) ** 2
         elif self.activation == 'relu':
@@ -228,10 +268,14 @@ class DenseLayer(Layer):
         elif self.activation == 'tanh':
             return np.sech(z) ** 2
         elif self.activation == 'softmax':
-            s = self._activate(z).reshape(-1, 1)
-            return np.diagflat(s) - np.dot(s, s.T)
+            # Jacobian of softmax is a diagonal matrix with the softmax values on the diagonal.
+            # https://eli.thegreenplace.net/2016/the-softmax-function-and-its-derivative/
+            return np.diag(z) - np.outer(z, z)
         elif self.activation == 'linear':
             return np.ones(z.shape)
+
+    def update_weights(self, Δw: np.ndarray):
+        self.weights += Δw
 
 class DropoutLayer(Layer):
     """

utilities/models/tests/test_nn.py
@@ -25,10 +25,10 @@ class TestNeuralNetwork(unittest.TestCase):
 
         np.random.seed(1)
 
-        model = NeuralNetworkModel(batch_size=9)
+        model = NeuralNetworkModel(batch_size=9, learning_rate=0.001, num_epochs=500)
         model.layers = [
             InputLayer(2),
-            DenseLayer(3, activation='sigmoid'),
+#             DenseLayer(3, activation='sigmoid'),
             DenseLayer(3, activation='sigmoid'),
             DenseLayer(3, activation='softmax'),
         ]